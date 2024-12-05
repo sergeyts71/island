f28ded355e9b7bdbdeaee3142fd7b35900698fa7
Sergei <sergeyts71@gmail.com>
2024-12-05 23:28:40 +0300

    ExecutorService service

--- src/App.java
+++ src/App.java
@@ -1,17 +1,40 @@
+import entity.IslandMap;
 import simulation.*;
-
+import java.util.concurrent.*;
 import java.util.concurrent.TimeUnit;
 
 public class App {
     public static void main(String[] args) {
-        MyRunnable myRunnable = new MyRunnable();
-        Thread myThread = new Thread(myRunnable);
-            myThread.start();
-        try {
-            TimeUnit.SECONDS.sleep(5);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+
+        ExecutorService service = Executors.newCachedThreadPool();
+
+        for(int i = 0; i < 10; i++) {
+
+            service.submit(new Runnable() {
+
+                public void run() {
+
+                    IslandMap islandMap = new IslandMap();
+                    Report report = new Report();
+
+    //                System.out.println(islandMap.getCell0());
+                    islandMap.eat("Заяц");
+                    System.out.println(islandMap.getCell0());
+
+                }
+
+            });
+
         }
-            myThread.stop();
+
+//        MyRunnable myRunnable = new MyRunnable();
+//        Thread myThread = new Thread(myRunnable);
+//            myThread.start();
+//        try {
+//            TimeUnit.SECONDS.sleep(5);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//            myThread.stop();
     }
 }
